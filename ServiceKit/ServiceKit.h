a49bbd1a5d856e98312db35d00c6726d89a8e859
Victor Shabanov <shabanov.dev.git@gmail.com>
2018-08-02 03:33:46 +0300

    Create project and setup initial project tree

---
+++ ServiceKit/ServiceKit.h
@@ -0,0 +1,19 @@
+//
+//  ServiceKit.h
+//  ServiceKit
+//
+//  Created by Victor Shabanov on 02/08/2018.
+//  Copyright © 2018 Victor Shabanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServiceKit.
+FOUNDATION_EXPORT double ServiceKitVersionNumber;
+
+//! Project version string for ServiceKit.
+FOUNDATION_EXPORT const unsigned char ServiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServiceKit/PublicHeader.h>
+
+
